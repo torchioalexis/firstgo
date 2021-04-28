7e599aeb063d38af2fe506d1d289f60f45c429d5
Alexis Torchio <torchio.alexis@gmail.com>
2021-04-28 01:00:34 -0300

    Modificacion del main y realización del primer reto con calculo de superficies

--- src/main.go
+++ src/main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("¡¡¡VAMOS CANAYA!!!")
+	// CALCULAR ÁREA RECTÁNGULO
+	baseRectangulo := 10
+	alturaRectangulo := 5
+	areaRectangulo := baseRectangulo * alturaRectangulo
+	fmt.Println("El área del rectángulo es:", areaRectangulo)
+
+	// CALCULAR ÁREA TRAPECIO
+	baseTrapecio1 := 10
+	baseTrapecio2 := 20
+	alturaTrapecio := 8
+	areaTrapecio := (baseTrapecio1 + baseTrapecio2) * alturaTrapecio / 2
+	fmt.Println("El área del trapecio es:", areaTrapecio)
+
+	// CALCULAR ÁREA CIRCULO
+	pi := 3.14
+	var radioCirculo float64 = 5
+	areaCirculo := pi * radioCirculo * radioCirculo
+	fmt.Println("El área del círculo es:", areaCirculo)
 }
